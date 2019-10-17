3b80a7ab2a5701b84edbaaa4ff109ac296bb2813
Lucas Madrazo <lucas.madrazo@mercadolibre.com>
2019-10-17 17:39:03 -0300

    Add sass color constants

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import '../styles/login.scss'
+
 
 /** Component that represent home screen  */
 const LoginPage = () => {
     return (
-        <div>     
+        <div class='login-page'>     
           login 
         </div>
     )
