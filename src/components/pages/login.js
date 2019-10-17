abb6edecbaa737b461aec1a64ee320eb2a90f883
Lucas Madrazo <lucas.madrazo@mercadolibre.com>
2019-10-17 20:59:25 -0300

    Add login styles

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -1,17 +1,34 @@
 import '../styles/login.scss'
 import React from 'react';
 import Card from '@andes/card';
-
+import Button from '@andes/button';
+import TextField from '@andes/textfield';
 
 /** Component that represent home screen  */
-const LoginPage = () => {
+
+class LoginPage extends React.Component {
+
+  handleLogin() {
+
+  }
+
+  render() {
     return (
-        <div class='login-page'>     
-          <Card className="login-page__form" paddingSize={0}>
-            <h1> Content</h1>
-          </Card>
-        </div>
+      <div className='login-page'>     
+        <Card className='login-page__card'>
+          <h1>Face Pay</h1>
+          <div className='login-page_fields'>
+            <TextField label="E-mail" message="Ej. ivael@facepay.com" />
+            <TextField label="Clave"/>
+          </div>
+          <div className="login-page__button">
+            <Button className="login-page__button--primary" size="large" onClick={this.handleLogin}>Ingresar</Button>
+            <Button disabled modifier="transparent">Crear cuenta</Button>
+          </div>
+        </Card>
+      </div>
     )
+  };
 }
 
 export default LoginPage;
