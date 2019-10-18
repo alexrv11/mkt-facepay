378699385c9c1e2970b3c0a2822a146d5a526cab
Juan Cerrutti <juan.cerrutti@mercadolibre.com>
2019-10-18 01:17:34 -0300

    more changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Helmet } from 'react-helmet';
 import LoginPage from './components/pages/login'
 import HomePage from './components/pages/home'
 import PayScreen from './components/pages/pay'
+import FaceRecognition from './components/pages/faceRecognition';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 import './App.scss';
 
@@ -24,6 +26,7 @@ function App() {
             <Route path="/" exact component={HomePage}/>
             <Route path="/login" exact component={LoginPage}/>
             <Route path="/pay" exact component={PayScreen}/>
+            <Route path="/face" exact component={FaceRecognition}/>
           </Switch>
         </div>  
       </BrowserRouter>
--- src/components/pages/faceRecognition.js
+++ src/components/pages/faceRecognition.js
@@ -1,33 +1,58 @@
 import React from "react";
 import * as faceapi from "face-api.js";
+import { parseBase64Image, authFace } from "../../services/faceIntegration";
+import "../styles/faceRecognition.scss";
+import Loader from "react-loader-spinner";
 
 let video;
 
 export default class FaceRecognition extends React.Component {
-  constructor() {
-    video = document.getElementById("video");
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      faceRecognized: false,
+      isPaying: false,
+      paymentOK: null
+    };
+  }
+
+  componentDidMount() {
+    this.initFaceRecognition();
+    video = document.getElementById("inputVideo");
 
     video.addEventListener("play", () => {
-      const canvas = faceapi.createCanvasFromMedia(video);
-      document.body.append(canvas);
-      const displaySize = { width: video.width, height: video.height };
-      faceapi.matchDimensions(canvas, displaySize);
-      setInterval(async () => {
-        const detections = await faceapi
-          .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-          .withFaceLandmarks()
-          .withFaceExpressions();
-        const resizedDetections = faceapi.resizeResults(
-          detections,
-          displaySize
-        );
-        canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-        faceapi.draw.drawDetections(canvas, resizedDetections);
-        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-      }, 100);
+      this.startRecognition();
     });
   }
+
+  startRecognition() {
+    const detectFaceInterval = setInterval(async () => {
+      const detections = await faceapi
+        .detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+      console.log("Detecting");
+      if (detections) {
+        // Face detected with high score
+        const score = detections.detection.score;
+        if (score >= 0.85 && !this.state.faceRecognized) {
+          console.log(detections.detection.score);
+          clearInterval(detectFaceInterval);
+          const imageCanvas = faceapi.createCanvasFromMedia(
+            document.getElementById("inputVideo"),
+            {
+              width: 480,
+              height: 272
+            }
+          );
+          debugger
+          this.faceRecognized(imageCanvas);
+        }
+      }
+    }, 200);
+  }
+
   async initFaceRecognition() {
     Promise.all([
       faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
@@ -35,6 +60,50 @@ export default class FaceRecognition extends React.Component {
       faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
       faceapi.nets.faceExpressionNet.loadFromUri("/models")
     ]).then(this.startVideo);
+  }
+
+  faceRecognized(imageCanvas) {
+    // Now we should call the api, or trigger the OK binding
+    this.setState({
+      faceRecognized: true
+    });
+    const imageBase64 = imageCanvas.toDataURL("image/jpeg");
+    const parsedImage = parseBase64Image(imageBase64);
+    this.requestPayment(parsedImage);
+    this.setState({
+      isPaying: true
+    });
+  }
+
+  requestPayment(face) {
+    authFace(face)
+      .then(res => {
+        console.log("successfull", res);
+        this.setState({
+          isPaying: false
+        });
+        this.setPaymentState(true);
+      })
+      .catch(err => {
+        console.log("Error", err);
+        this.setState({
+          isPaying: false
+        });
+        this.setPaymentState(false);
+      });
+  }
+
+  setPaymentState(state) {
+    this.setState({
+      paymentOK: state
+    });
+    setTimeout(() => {
+      this.setState({
+        paymentOK: null,
+        faceRecognized: false,
+      });
+      this.startRecognition();
+    }, 2000);
   }
 
   startVideo() {
@@ -46,17 +115,35 @@ export default class FaceRecognition extends React.Component {
   }
 
   render() {
+    const { isPaying, paymentOK } = this.state;
     return (
       <div className="face-recognition-container">
-        <video
-          width="480"
-          height="480"
-          id="inputVideo"
-          autoplay
-          muted
-          onloadedmetadata="onPlay(this)"
-          playsinline
-        />
+        <div className="video-container">
+          {isPaying && (
+            <Loader
+              className="loader"
+              type="Oval"
+              color="#00BFFF"
+              height={480}
+              width={480}
+            />
+          )}
+          {paymentOK === true && (
+            <Loader className="loader" type="Puff" color="#008744" height={480} width={480} />
+          )}
+          {paymentOK === false && (
+            <Loader className="loader" type="Puff" color="#D12440" height={480} width={480} />
+          )}
+          <video
+            className="video-element"
+            width="480"
+            height="480"
+            id="inputVideo"
+            autoPlay
+            muted
+            playsInline
+          />
+        </div>
       </div>
     );
   }
