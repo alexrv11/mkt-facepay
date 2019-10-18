edaab9b1ae3d00c74a5b44b23b9c738ffa0598cc
Alex Ventura <alex.rv11@gmail.com>
2019-10-18 05:38:17 -0300

    Updates

---
+++ precache-manifest.4246e7ec0fc5366c01811750d87da580.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4ed326895848898ef574c5fe2147f4",
+    "url": "/mkt-facepay/index.html"
+  },
+  {
+    "revision": "06af830d43748d9e369d",
+    "url": "/mkt-facepay/static/css/2.aba131b8.chunk.css"
+  },
+  {
+    "revision": "a229f8291f672eaf354c",
+    "url": "/mkt-facepay/static/css/main.f8f74f8c.chunk.css"
+  },
+  {
+    "revision": "06af830d43748d9e369d",
+    "url": "/mkt-facepay/static/js/2.6f24e01d.chunk.js"
+  },
+  {
+    "revision": "a229f8291f672eaf354c",
+    "url": "/mkt-facepay/static/js/main.2fac2f74.chunk.js"
+  },
+  {
+    "revision": "1bd780f45679662a6b3b",
+    "url": "/mkt-facepay/static/js/runtime-main.9846e2fc.js"
+  },
+  {
+    "revision": "73ff319e07c0a52f75c7ed3e7547e378",
+    "url": "/mkt-facepay/static/media/intro.73ff319e.gif"
+  },
+  {
+    "revision": "4dd19aad49fd66b40bfa9b4afb4841a2",
+    "url": "/mkt-facepay/static/media/logo.4dd19aad.png"
+  },
+  {
+    "revision": "8e016f903ade805a2dc1f06d9dfbadf0",
+    "url": "/mkt-facepay/static/media/wick-kraken-logo-invert.8e016f90.png"
+  },
+  {
+    "revision": "91ac24f4c7b0a965a0236b0e234b9be8",
+    "url": "/mkt-facepay/static/media/wick-kraken-logo.91ac24f4.png"
+  }
+]);
